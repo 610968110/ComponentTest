af9c608934afcc211cfb7631199fab8aee40e4e7
614666808 <610968110@qq.com>
2018-12-13 13:34:18 +0800

    完善项目

--- main/src/main/java/debug/MainApplication.java
+++ main/src/main/java/debug/MainApplication.java
@@ -1,6 +1,5 @@
-package com.lbx.componenttest;
+package debug;
 
-import com.alibaba.android.arouter.launcher.ARouter;
 import com.lbx.lib_common.BaseApplication;
 
 /**
@@ -23,22 +22,12 @@ import com.lbx.lib_common.BaseApplication;
  * .      ┗┻┛　┗┻┛
  *
  * @author lbx
- * @date 2018/12/10.
+ * @date 2018/12/11.
  */
 
-public class MainApplication extends BaseApplication {
-
-
+public class MainApplacation extends BaseApplication {
     @Override
     public void onCreate(boolean isDebug) {
-        // 这两行必须写在init之前，否则这些配置在init过程中将无效
-        if (isDebug) {
-            // 打印日志
-            ARouter.openLog();
-            // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
-            ARouter.openDebug();
-        }
-        // 尽可能早，推荐在Application中初始化
-        ARouter.init(this);
+
     }
 }
